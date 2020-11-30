a66b6c21a5fcf5e8fba16ddcdcf9417e5f953d0f
panyexiong <panyexiong@xinye.com>
2020-11-30 20:59:54 +0800

    add PostController

---
+++ src/main/java/com/panyexiong/blog/controller/PostController.java
@@ -0,0 +1,17 @@
+package com.panyexiong.blog.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("post")
+public class PostController {
+
+
+    @PostMapping("/obj")
+    @ResponseBody
+    public Object returnJson(@RequestBody Object obj){
+        System.out.println(obj);
+        return obj;
+    }
+
+}
